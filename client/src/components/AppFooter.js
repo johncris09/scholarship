d4f1cbdf771539e5caef65ca59c38a2545f54d07
John Cris Manabo <johncrismanabo9@gmail.com>
2024-03-08 10:37:27 +0800

    move the current sem and sy from header to footer

--- client/src/components/AppFooter.js
+++ client/src/components/AppFooter.js
@@ -1,10 +1,46 @@
+import React, { useEffect, useState } from 'react'
 import { CFooter } from '@coreui/react'
 import { AlternateEmailRounded } from '@mui/icons-material'
-import React from 'react'
+import { api } from './SystemConfiguration'
+import { Skeleton } from '@mui/material'
 const AppFooter = () => {
+  const [config, setConfig] = useState([])
+  const [fetchConfigLoading, setFetchConfigLoading] = useState(false)
+
+  const [sy, setSY] = useState('')
+  const [semester, setSemester] = useState('')
   const currentYear = new Date().getFullYear()
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = () => {
+    api
+      .get('config')
+      .then((response) => {
+        setSemester(response.data[0].current_semester)
+        setSY(response.data[0].current_sy.replace(/SY: /g, ''))
+      })
+      .catch((error) => {
+        console.info(error)
+        // toast.error(handleError(error))
+      })
+      .finally(() => {
+        setFetchConfigLoading(false)
+      })
+  }
+
   return (
     <CFooter>
+      <div className="mr-auto">
+        {!fetchConfigLoading ? (
+          <span className="_currentView">
+            School Year: <strong>{sy}</strong> | Semester: <strong>{semester}</strong>
+          </span>
+        ) : (
+          <Skeleton variant="rectangular" width={250} />
+        )}
+      </div>
       <div className="ms-auto">
         <span className="me-1">Powered by</span>
         <a href="https://oroquietacity.net/" target="_blank" rel="noopener noreferrer">
