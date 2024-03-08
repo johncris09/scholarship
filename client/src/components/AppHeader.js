ede561f4d4ab6e7d951b410d94908c126df57803
John Cris Manabo <johncrismanabo9@gmail.com>
2024-03-08 10:36:53 +0800

    add search feature

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -1,46 +1,65 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import './../assets/css/custom.css'
 import {
   CContainer,
   CHeader,
-  CHeaderBrand,
   CHeaderDivider,
   CHeaderNav,
   CHeaderToggler,
+  CSpinner,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import { cilMenu } from '@coreui/icons'
-
+import { cilMenu, cilX } from '@coreui/icons'
 import { AppBreadcrumb } from './index'
 import { AppHeaderDropdown } from './header/index'
-import { Skeleton, Typography } from '@mui/material'
-import { DefaultLoading, api } from './SystemConfiguration'
+import { api, toSentenceCase } from './SystemConfiguration'
+import { AsyncTypeahead } from 'react-bootstrap-typeahead'
+import { useNavigate } from 'react-router-dom'
 
 const AppHeader = () => {
-  const [config, setConfig] = useState([])
-  const [fetchConfigLoading, setFetchConfigLoading] = useState(false)
+  const typeAheadRef = useRef()
   const dispatch = useDispatch()
   const sidebarShow = useSelector((state) => state.sidebarShow)
-  useEffect(() => {
-    fetchData()
-  }, [])
 
-  const fetchData = () => {
-    api
-      .get('config')
+  const [isLoading, setIsLoading] = useState(false)
+  const [options, setOptions] = useState([])
+
+  const navigate = useNavigate()
+
+  const handleSearch = async (query) => {
+    setIsLoading(true)
+
+    await api
+      .get('advance_search', { params: { query } })
       .then((response) => {
-        setConfig(response.data[0])
+        setOptions(response.data)
       })
       .catch((error) => {
         console.info(error)
-        // toast.error(handleError(error))
       })
       .finally(() => {
-        setFetchConfigLoading(false)
+        setIsLoading(false)
       })
   }
+  const filterBy = () => true
 
+  const handleSelection = (selected) => {
+    if (selected && selected.length > 0 && selected[0].firstname) {
+      // Access the firstname property here
+      navigate(
+        '/search/' +
+          toSentenceCase(selected[0].lastname) +
+          ', ' +
+          toSentenceCase(selected[0].firstname) +
+          ' ' +
+          toSentenceCase(selected[0].middlename) +
+          ' ' +
+          toSentenceCase(selected[0].suffix),
+        { replace: true },
+      )
+    }
+  }
   return (
     <CHeader position="sticky" className="mb-4">
       <CContainer fluid>
@@ -50,32 +69,88 @@ const AppHeader = () => {
         >
           <CIcon icon={cilMenu} size="lg" />
         </CHeaderToggler>
-
-        <CHeaderBrand className="toggleHide _currentView" style={{ fontSize: 15 }}>
-          <>
-            {!fetchConfigLoading ? (
-              <>
-                School Year: {config.current_sy} | Semester: {config.current_semester}
-              </>
-            ) : (
-              <Skeleton variant="rectangular" width={360} />
+        <CHeaderNav className="me-auto" id="async-search" style={{ position: 'relative' }}>
+          <AsyncTypeahead
+            ref={typeAheadRef}
+            align="justify"
+            filterBy={filterBy}
+            // isLoading={isLoading}
+            labelKey={(option) =>
+              `${toSentenceCase(option.lastname)}, ${toSentenceCase(
+                option.firstname,
+              )} ${toSentenceCase(option.middlename)} ${option.suffix}`
+            }
+            highlightClassName="rbt-highlight-text"
+            minLength={2}
+            highlightOnlyResult={true}
+            autoFocus={true}
+            onSearch={handleSearch}
+            onChange={handleSelection}
+            options={options}
+            maxResults={5}
+            placeholder="Search applicant ..."
+            inputProps={{ style: { paddingLeft: 40, borderRadius: 20 } }}
+            renderMenuItemChildren={(row) => (
+              <span>
+                {toSentenceCase(row.lastname)}, {toSentenceCase(row.firstname)}{' '}
+                {toSentenceCase(row.middlename)} {row.suffix}
+              </span>
             )}
-          </>
-        </CHeaderBrand>
-        <CHeaderBrand className="mx-auto  d-md-none _currentView" to="/#">
-          <Typography variant="h6">Oroquieta City Scholarship System</Typography>
-          <Typography variant="h6" className="current-view">
-            <>
-              {!fetchConfigLoading ? (
-                <>
-                  School Year: {config.current_sy} | Semester: {config.current_semester}
-                </>
-              ) : (
-                <Skeleton variant="rectangular" width={250} />
-              )}
-            </>
-          </Typography>
-        </CHeaderBrand>
+            renderToken={(option, props, index) => (
+              <span key={index}>
+                {`${toSentenceCase(option.lastname)}, ${toSentenceCase(
+                  option.firstname,
+                )} ${toSentenceCase(option.middlename)} ${option.suffix}`}
+              </span>
+            )}
+          />
+          {!isLoading ? (
+            <lord-icon
+              src="https://cdn.lordicon.com/kkvxgpti.json"
+              trigger="hover"
+              delay="2000"
+              colors="primary:#b4b4b4"
+              style={{
+                width: '30px',
+                height: '30px',
+                position: 'absolute',
+                top: '50%',
+                left: '5px',
+                transform: 'translateY(-50%)',
+              }}
+            ></lord-icon>
+          ) : (
+            <CSpinner
+              size="sm"
+              style={{
+                width: '20px',
+                height: '20px',
+                position: 'absolute',
+                top: '20%',
+                left: '10px',
+                color: '#b4b4b4',
+                // transform: 'translateY(-50%)',
+              }}
+            />
+          )}
+          {options.length > 0 && (
+            <CIcon
+              icon={cilX}
+              onClick={() => {
+                setOptions([])
+                typeAheadRef.current.clear()
+              }}
+              style={{
+                position: 'absolute',
+                color: '#b4b4b4',
+                top: '50%',
+                right: '15px',
+                transform: 'translateY(-50%)',
+              }}
+            />
+          )}
+        </CHeaderNav>
+
         <CHeaderNav className="ms-3">
           <AppHeaderDropdown />
         </CHeaderNav>
